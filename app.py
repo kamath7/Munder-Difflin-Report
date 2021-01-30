4595e1ac312e0ed747bebf3a1ef2984917aa092c
kamath7 <adithyakamath96@gmail.com>
2021-01-30 21:56:13 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Welcome to Munder Difflin Paper")
+
+
+client_email = input('Enter the client\'s email address: ')
+client_name = input('Enter the client\'s name: ')
+client_address = input('Enter the client\'s address: ')
+description_sale = input('Enter the sale description: ')
+cost = input('Enter the cost of sale: ')
+inv_number = input('Enter the invoice number: ')
+today_date = input('Enter today\'s date: ')
+due_date = input('Enter the due date for the invoice: ')
+
+
+print(client_email,client_name,client_address,description_sale,cost,inv_number,today_date,due_date)
