78d2cd9f40ad9f7a22b34c3baddd1ffd679a1c97
kamath7 <adithyakamath96@gmail.com>
2021-02-01 03:05:46 +0530

    Filled the template

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-print("Welcome to Munder Difflin Paper")
+from openpyxl import Workbook, load_workbook
+import pandas as pd
 
+print("Welcome to Munder Difflin Paper")
+wb = load_workbook('Munder Difflin.xlsx')
+
+sheet = wb.active
 
 client_email = input('Enter the client\'s email address: ')
 client_name = input('Enter the client\'s name: ')
@@ -8,7 +13,16 @@ description_sale = input('Enter the sale description: ')
 cost = input('Enter the cost of sale: ')
 inv_number = input('Enter the invoice number: ')
 today_date = input('Enter today\'s date: ')
-due_date = input('Enter the due date for the invoice: ')
+due_date = input('Enter the due date for  the invoice: ')
 
 
-print(client_email,client_name,client_address,description_sale,cost,inv_number,today_date,due_date)
+sheet.cell(row=10, column=2).value = client_name
+sheet.cell(row=10, column=4).value = client_address
+sheet.cell(row=9, column=6).value = inv_number
+sheet.cell(row=10, column=6).value = today_date
+sheet.cell(row=11, column=6).value = due_date
+sheet.cell(row=16, column=2).value = description_sale
+sheet.cell(row=16, column=5).value = cost
+
+
+wb.save('Dunds.xlsx')
